012b0ec29532f6c1ca9b8ca06466f3a818e1b3dd
Harshit Dwivedi <harshithdwivedi@gmail.com>
2017-10-28 23:19:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,6 @@ SECRET_KEY = 'test_key'
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
-    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
